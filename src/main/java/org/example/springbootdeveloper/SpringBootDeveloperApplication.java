5ad5dfe0331ab1d81e8e529fcf6cd6cb5ab2d3c0
ymiye <ymiye1109@gmail.com>
2023-11-04 15:26:37 +0900

    11/04 Blog 만들기_1

---
+++ src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -0,0 +1,12 @@
+package org.example.springbootdeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SpringBootDeveloperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDeveloperApplication.class, args);
+    }
+}
