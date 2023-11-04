a3f8579510dcd0322d8a359c40345a1128581079
ymiye <ymiye1109@gmail.com>
2023-11-05 00:28:24 +0900

    11/05 Blog 만들기_2

--- src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
+++ src/main/java/org/example/springbootdeveloper/SpringBootDeveloperApplication.java
@@ -3,7 +3,9 @@ package org.example.springbootdeveloper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringBootDeveloperApplication {
     public static void main(String[] args) {
--- src/main/java/org/example/springbootdeveloper/domain/Article.java
+++ src/main/java/org/example/springbootdeveloper/domain/Article.java
@@ -2,6 +2,10 @@ package org.example.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -27,4 +31,12 @@ public class Article {
         this.title = title;
         this.content = content;
     }
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
